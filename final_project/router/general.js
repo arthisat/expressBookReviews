3f6399637712844f3883b6b0d82737f1d9afbf49
arthisat <art.sathian@gmail.com>
2026-01-18 12:44:02 -0500

    add 3 GET methods in general.js

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -12,20 +12,31 @@ public_users.post("/register", (req,res) => {
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    // send JSON response with formatted book list data
+    return res.status(200).send(JSON.stringify(books, null, 4));
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    // retrieve the ISBN param from the request URL and send specific book
+    const isbn = req.params.isbn;
+    return res.status(200).send(books[isbn]);
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    // set up author constant for comparison and empty array for returning
+    const author = req.params.author;
+    let filteredBooks = {};
+
+    // check all books, if author matches, put the book in the filteredBooks array
+    for (let isbn in books) {
+        if (books[isbn].author === author) {
+            filteredBooks[isbn] = books[isbn];
+        }
+    }
+
+    return res.status(200).send(filteredBooks);
 });
 
 // Get all books based on title
